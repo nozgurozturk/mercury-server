0b1cd98a6e40726ad5566dde1ca61119ac4f3d18
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-09-22 12:35:01 +0300

    Add basic user handler without auth

---
+++ src/handlers/userHandler.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+"encoding/json"
+"models"
+"net/http"
+
+"github.com/gorilla/mux"
+)
+
+var users []models.User
+
+var CreateUser= func(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	var user models.User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+var GetUsers= func(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "aplication/json")
+	json.NewEncoder(w).Encode(users)
+}
+var GetUser= func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "aplication/json")
+	params := mux.Vars(r)
+	for _, user := range users {
+		if user.ID == params["id"] {
+			json.NewEncoder(w).Encode(users)
+		}
+	}
+	json.NewEncoder(w).Encode(&models.User{})
+}
+var UpdateUser= func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "aplication/json")
+	params := mux.Vars(r)
+	for index, item := range users {
+		if item.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			var user models.User
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			user.ID = item.ID
+			users = append(users, user)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+var DeleteUser= func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "aplication/json")
+	params := mux.Vars(r)
+	for index, item := range users {
+		if item.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
