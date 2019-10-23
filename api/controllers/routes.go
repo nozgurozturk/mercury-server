12f53ea12f3812457f00ad78a2e1264ed43f0273
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-10-23 20:34:21 +0300

    Update routes direction

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -26,8 +26,8 @@ func (server *Server) userRoutes() {
 func (server *Server) boardRoutes() {
 
 	server.Router.HandleFunc("/board", server.CreateBoard).Methods("POST")
-	server.Router.HandleFunc("/me/board", server.GetBoards).Methods("GET")
-	server.Router.HandleFunc("/me/board/{id}", server.GetBoard).Methods("GET")
+	server.Router.HandleFunc("/board", server.GetBoards).Methods("GET")
+	server.Router.HandleFunc("/board/{id}", server.GetBoard).Methods("GET")
 	server.Router.HandleFunc("/board/{id}", server.UpdateBoard).Methods("PUT")
 	server.Router.HandleFunc("/board/{id}", server.DeleteBoard).Methods("DELETE")
 
@@ -36,8 +36,8 @@ func (server *Server) boardRoutes() {
 func (server *Server) itemRoutes() {
 
 	server.Router.HandleFunc("/item", server.CreateItem).Methods("POST")
-	server.Router.HandleFunc("/me/item", server.GetItems).Methods("GET")
-	server.Router.HandleFunc("/me/item/{id}", server.GetItem).Methods("GET")
+	server.Router.HandleFunc("/item", server.GetItems).Methods("GET")
+	server.Router.HandleFunc("/item/{id}", server.GetItem).Methods("GET")
 	server.Router.HandleFunc("/item/{id}", server.UpdateItem).Methods("PUT")
 	server.Router.HandleFunc("/item/{id}", server.DeleteItem).Methods("DELETE")
 
@@ -46,8 +46,8 @@ func (server *Server) itemRoutes() {
 func (server *Server) linkRoutes() {
 
 	server.Router.HandleFunc("/link", server.CreateLink).Methods("POST")
-	server.Router.HandleFunc("/me/link", server.GetLinks).Methods("GET")
-	server.Router.HandleFunc("/me/link/{id}", server.GetLink).Methods("GET")
+	server.Router.HandleFunc("/link", server.GetLinks).Methods("GET")
+	server.Router.HandleFunc("/link/{id}", server.GetLink).Methods("GET")
 	server.Router.HandleFunc("/link/{id}", server.UpdateLink).Methods("PUT")
 	server.Router.HandleFunc("/link/{id}", server.DeleteLink).Methods("DELETE")
 
