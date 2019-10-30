200038f02533aeb53ce277d246804cfaec25e0f0
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-10-30 22:21:43 +0300

    Update : http methods for cors middleware

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,5 +1,7 @@
 package controllers
 
+import "net/http"
+
 func (server *Server) initializeRoutes() {
 	server.loginRoute()
 	server.userRoutes()
@@ -9,46 +11,46 @@ func (server *Server) initializeRoutes() {
 }
 
 func (server *Server) loginRoute() {
-	server.Router.HandleFunc("/", server.Home).Methods("GET")
-	server.Router.HandleFunc("/login", server.Login).Methods("POST")
-	server.Router.HandleFunc("/signup", server.SignUp).Methods("POST")
+	server.Router.HandleFunc("/", server.Home).Methods(http.MethodGet)
+	server.Router.HandleFunc("/login", server.Login).Methods(http.MethodPost)
+	server.Router.HandleFunc("/signup", server.SignUp).Methods(http.MethodPost)
 
 }
 
 func (server *Server) userRoutes() {
 
-	server.Router.HandleFunc("/user/{id}", server.GetUser).Methods("GET")
-	server.Router.HandleFunc("/user/{id}", server.UpdateUser).Methods("PUT")
-	server.Router.HandleFunc("/user/{id}", server.DeleteUser).Methods("DELETE")
+	server.Router.HandleFunc("/user/{id}", server.GetUser).Methods(http.MethodGet)
+	server.Router.HandleFunc("/user/{id}", server.UpdateUser).Methods(http.MethodPut)
+	server.Router.HandleFunc("/user/{id}", server.DeleteUser).Methods(http.MethodDelete)
 
 }
 
 func (server *Server) boardRoutes() {
 
-	server.Router.HandleFunc("/board", server.CreateBoard).Methods("POST")
-	server.Router.HandleFunc("/board", server.GetBoards).Methods("GET")
-	server.Router.HandleFunc("/board/{id}", server.GetBoard).Methods("GET")
-	server.Router.HandleFunc("/board/{id}", server.UpdateBoard).Methods("PUT")
-	server.Router.HandleFunc("/board/{id}", server.DeleteBoard).Methods("DELETE")
+	server.Router.HandleFunc("/board", server.CreateBoard).Methods(http.MethodPost)
+	server.Router.HandleFunc("/board", server.GetBoards).Methods(http.MethodGet)
+	server.Router.HandleFunc("/board/{id}", server.GetBoard).Methods(http.MethodGet)
+	server.Router.HandleFunc("/board/{id}", server.UpdateBoard).Methods(http.MethodPut)
+	server.Router.HandleFunc("/board/{id}", server.DeleteBoard).Methods(http.MethodDelete)
 
 }
 
 func (server *Server) itemRoutes() {
 
-	server.Router.HandleFunc("/item", server.CreateItem).Methods("POST")
-	server.Router.HandleFunc("/board/{id}/item", server.GetItems).Methods("GET")
-	server.Router.HandleFunc("/item/{id}", server.GetItem).Methods("GET")
-	server.Router.HandleFunc("/item/{id}", server.UpdateItem).Methods("PUT")
-	server.Router.HandleFunc("/item/{id}", server.DeleteItem).Methods("DELETE")
+	server.Router.HandleFunc("/item", server.CreateItem).Methods(http.MethodPost)
+	server.Router.HandleFunc("/board/{id}/item", server.GetItems).Methods(http.MethodGet)
+	server.Router.HandleFunc("/item/{id}", server.GetItem).Methods(http.MethodGet)
+	server.Router.HandleFunc("/item/{id}", server.UpdateItem).Methods(http.MethodPut)
+	server.Router.HandleFunc("/item/{id}", server.DeleteItem).Methods(http.MethodDelete)
 
 }
 
 func (server *Server) linkRoutes() {
 
-	server.Router.HandleFunc("/link", server.CreateLink).Methods("POST")
-	server.Router.HandleFunc("/items/{id}/link", server.GetLinks).Methods("GET")
-	server.Router.HandleFunc("/link/{id}", server.GetLink).Methods("GET")
-	server.Router.HandleFunc("/link/{id}", server.UpdateLink).Methods("PUT")
-	server.Router.HandleFunc("/link/{id}", server.DeleteLink).Methods("DELETE")
+	server.Router.HandleFunc("/link", server.CreateLink).Methods(http.MethodPost)
+	server.Router.HandleFunc("/items/{id}/link", server.GetLinks).Methods(http.MethodGet)
+	server.Router.HandleFunc("/link/{id}", server.GetLink).Methods(http.MethodGet)
+	server.Router.HandleFunc("/link/{id}", server.UpdateLink).Methods(http.MethodPut)
+	server.Router.HandleFunc("/link/{id}", server.DeleteLink).Methods(http.MethodDelete)
 
 }
